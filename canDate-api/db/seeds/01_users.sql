8ed34715840948eaabba9c4de55e52e5286aa97e
Zio <zi.ao.tan@outlook.com>
2021-07-01 16:32:56 +0000

    Add more columns in users table

--- canDate-api/db/seeds/01_users.sql
+++ canDate-api/db/seeds/01_users.sql
@@ -1,5 +1,21 @@
 -- Users table seeds here (Example)
-INSERT INTO users(username, email, password) VALUES ('Khea', 'khea@email.com', 'khea');
-INSERT INTO users(username, email, password) VALUES ('Zio', 'zio@email.com', 'zio');
-INSERT INTO users(username, email, password) VALUES ('Matt', 'matt@email.com', 'matt');
+INSERT INTO users(username, email, password, gender, address, about_me)
+VALUES ('Khea', 'khea@email.com', 'khea', 'female', 'Vancouver', 'I like Lighthouse labs' );
+
+
+INSERT INTO users(username, email, password, gender, address, about_me)
+VALUES ('Zio', 'zio@email.com', 'zio', 'male', 'Surrey', 'I like DC comics' );
+
+INSERT INTO users(username, email, password, gender, address, about_me)
+VALUES ('Matt', 'matt@email.com', 'matt', 'male', 'Richmond', 'I like Lego');
+
+INSERT INTO users(username, email, password, gender, height, address, age, profile_photo, about_me)
+VALUES ('Tim', 'tim@email.com', 'tim', 'male', 180, 'Calgary', 25, 'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg', 'I like cooking');
+
+INSERT INTO users(username, email, password, gender, height, address, age, profile_photo, about_me)
+VALUES ('Chris', 'chris@email.com', 'chris', 'male', 190, 'Burnaby', 27, 'https://lh3.googleusercontent.com/proxy/WddGp5g5vkM_zvUk2KSdTJVJAZC2bWqqVaqguoZEZzZgxl7j1rB-jcL3NrP5S0guh63b_5tA9mdWOa2sJkYdlEBTGf2L9VMRXszamO6gQ6WrmVv6XrmOIirdvYZnKg', 'I like hiking');
+
+INSERT INTO users(username, email, password, gender, height, address, age, profile_photo, about_me)
+VALUES ('Kelly', 'kelly@email.com', 'kelly',  'female', 165, 'Burnaby', 23, 'https://www.theattitudequotes.com/wp-content/uploads/2021/02/Best-DP-For-Girls-WhatsApp-Profile-Picture-2021-Girls-WhatsApp-Dp-35.jpg', 'I like dancing');
+
 
