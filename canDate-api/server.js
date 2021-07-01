bf7d42ea654088e760b7fa4468902a121008cc31
Zio <zi.ao.tan@outlook.com>
2021-07-01 00:32:21 +0000

    clean up unuseful code

--- canDate-api/server.js
+++ canDate-api/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
-const sass = require("node-sass-middleware");
+
 const app = express();
 const morgan = require("morgan");
 const db = require("./lib/connection.js");
@@ -14,27 +14,13 @@ db.connect();
 
 app.use(morgan("dev"));
 
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  "/styles",
-  sass({
-    src: __dirname + "/styles",
-    dest: __dirname + "/public/styles",
-    debug: true,
-    outputStyle: "expanded",
-  })
-);
+
 app.use(express.static("public"));
 
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
