865653e548827db60196dc106ebb77b02ae296c6
MattLuo90 <mattluo604@hotmail.com>
2021-07-02 15:35:29 -0700

    new change

---
+++ react-front-end/src/Components/TagListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+
+
+
+const TagListItem = props => {
+
+  return (
+
+    <Button
+      onClick={() => { }}
+      type="submit"
+      variant="contained"
+      color="withe"
+    >{props.name}</Button>
+
+  )
+}
+
+
+export default TagListItem;
