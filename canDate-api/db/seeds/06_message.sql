e5c8dc9b98b34294b7e47e939c3df373854bdab6
Zio7711 <zi.ao.tan@outlook.com>
2021-07-02 17:12:18 -0700

    modifie database

--- canDate-api/db/seeds/06_message.sql
+++ canDate-api/db/seeds/06_message.sql
@@ -4,3 +4,12 @@ INSERT INTO message(to_user_id, from_user_id, content) VALUES (1, 2, 'I like you
 INSERT INTO message(to_user_id, from_user_id, content) VALUES (2, 1, 'Hello');
 INSERT INTO message(to_user_id, from_user_id, content) VALUES (2, 1, 'This is Zio');
 INSERT INTO message(to_user_id, from_user_id, content) VALUES (2, 1, 'I like you');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (1, 3, 'Testing');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (1, 3, 'You are so pretty');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (1, 3, 'You are awesome');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (1, 3, 'I hate you');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (3, 1, 'Never mind');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (3, 1, 'Do not message me');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (3, 1, 'See you');
+INSERT INTO message(to_user_id, from_user_id, content) VALUES (3, 1, 'Bye');
+
