f9b7edaaa496d695ec3ee12d5d4751850c18646e
Khea Chang <kheacyc@gmail.com>
2021-07-06 04:46:51 +0800

    show filtered user profiles

--- react-front-end/src/Components/UserPage.js
+++ react-front-end/src/Components/UserPage.js
@@ -62,18 +62,67 @@ const UserPage = (props) => {
     setSelectTag(selectArr)
   };
 
+  // returns filtered array of unique userids 
+  const getFilteredUsers = (interests, user_tagArr) => {
+    const users = [];
+    for (const interest of interests) {
+      for (const user of user_tagArr) {
+        if (interest === user.interest) {
+          users.push(user.id)
+        }
+      }
+    }
+  
+    let removeRepeatedUsers = [];
+    users.forEach(element => {
+      if (!removeRepeatedUsers.includes(element)) {
+        removeRepeatedUsers.push(element)
+      }
+    })
+    return removeRepeatedUsers; 
+  }
+  
+  // return array of objects for filtered users
+  const getFilteredUserProfile = (filteredUserId, users) => {
+    const userProfiles = [];
+    for (const user of users) {
+      for (const userId of filteredUserId) {
+        if (user.id === userId) {
+          userProfiles.push(user)
+        }
+      }
+    }
+    return userProfiles
+  }
+  
   return (
     <>
       <TagsContext.Provider values={tag}>
         <NavBar handleTagClick={handleTagClick}/>
       </TagsContext.Provider>
       <div>
-        {state.users.map((filteredUser) => {
+        {selectTag === [] ? state.users.map((user) => {
           return (
             <Grid container spacing={4}>
               <Grid item xs={12} sm={6} md={3}>
                 <ProfileCard
-                  key={filteredUser.id}
+                  key={user.id}
+                  name={user.first_name}
+                  age={user.age}
+                  profile_photo={user.profile_photo}
+                  tag={getNameOfTag(filterTags(user.id, user_tag), tag)}
+                />
+              </Grid>
+            </Grid>
+          );
+        }) 
+        // [1, 2, 3, 4]
+        : getFilteredUserProfile(getFilteredUsers(selectTag, user_tag), state.users).map((filteredUser) => {
+          return (
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={6} md={3}>
+                <ProfileCard
+                  key={filteredUser}
                   name={filteredUser.first_name}
                   age={filteredUser.age}
                   profile_photo={filteredUser.profile_photo}
@@ -82,7 +131,8 @@ const UserPage = (props) => {
               </Grid>
             </Grid>
           );
-        })}
+        })
+      }
       </div>
       {console.log("state.users", state.users)}
       {console.log("clicked", selectTag)}
