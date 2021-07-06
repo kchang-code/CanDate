3fa00f5ab33d59691a7a8a5dbb332af98bdfccfb
Khea Chang <kheacyc@gmail.com>
2021-07-06 13:01:45 +0800

    remove users prop in profilecards

--- react-front-end/src/Components/UserPage.js
+++ react-front-end/src/Components/UserPage.js
@@ -74,7 +74,6 @@ const UserPage = (props) => {
                 <Grid container spacing={4}>
                   <Grid item xs={12} sm={6} md={3}>
                     <ProfileCard
-                      users={users}
                       key={filteredUser.id}
                       name={filteredUser.first_name}
                       age={filteredUser.age}
