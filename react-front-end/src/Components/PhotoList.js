0131fbdd013dd91ac0e265ddeb0aa0f2a3a9e132
MattLuo90 <mattluo604@hotmail.com>
2021-07-06 22:13:38 -0700

    clean up database

--- react-front-end/src/Components/PhotoList.js
+++ react-front-end/src/Components/PhotoList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Photo from './Photo';
 
 export default function PhotoList(props) {
-  const imageArr = props.image.slice(40, 75)
-  const lists = imageArr.map((item) => {
+  // const imageArr = props.image.slice(40, 75)
+  const lists = props.image.map((item) => {
     return (
       <Photo image={item.profile_photo} />
     );
