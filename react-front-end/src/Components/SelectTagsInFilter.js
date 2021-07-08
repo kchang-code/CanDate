7a763e27d95ed10f5ecbe0b1f0041f998144cde1
Zio7711 <zi.ao.tan@outlook.com>
2021-07-07 18:01:10 -0700

    fix favorite user bug

--- react-front-end/src/Components/SelectTagsInFilter.js
+++ react-front-end/src/Components/SelectTagsInFilter.js
@@ -1,25 +1,30 @@
-import React, {useState} from 'react';
-import Button from "@material-ui/core/Button";
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 import className from 'classnames';
 
-export default function SelectTagsInFilter (props) {
+export default function SelectTagsInFilter(props) {
+  // const [selected, setSelected] = useState(false);
 
   return (
-    
     <div>
-
       <h3>Interests:</h3>
-    {props.tags.map((item) => {
-      return (
-        <Button 
-        // className={tagButtonColor === true  ? "tags" : "tags-selected" }
-        onClick={() => props.handleTagClick(item.id)} 
-        size="small" variant="outlined" color="primary">
-          {item.name}
-        </Button>
-      );
-    })}
-    {/* {console.log("clicked", selectTag)} */}
+      {props.tags.map((item) => {
+        return (
+          <Button
+            // className={tagButtonColor === true  ? "tags" : "tags-selected" }
+            style={{ backgroundColor: 'color' }}
+            onClick={() => {
+              props.handleTagClick(item.id);
+            }}
+            size="small"
+            variant="outlined"
+            color="primary"
+          >
+            {item.name}
+          </Button>
+        );
+      })}
+      {/* {console.log("clicked", selectTag)} */}
     </div>
-  )
+  );
 }
