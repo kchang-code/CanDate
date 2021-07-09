2208b0794707588125955bd7c26b3dd69436ebc0
Khea Chang <kheacyc@gmail.com>
2021-07-09 12:48:47 -0700

    add styling of filter buttons

--- react-front-end/src/Components/SelectCityInFilter.js
+++ react-front-end/src/Components/SelectCityInFilter.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ButtonBase } from '@material-ui/core';
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+  crossorigin="anonymous"
+/>
 export default function SelectCityInFilter(props) {
   const [newColor, setNewColor] = useState('grey')
   const [select, setSelect] = useState(false)
@@ -15,9 +23,9 @@ export default function SelectCityInFilter(props) {
     }
   }
   return (
-    <button
+    <Button
       onClick={() => { props.handleAddressClick(props.city); cityColor(select) }}
       style={{ backgroundColor: newColor }}
-    >{props.city}</button>
+    >{props.city}</Button>
   );
 }
--- react-front-end/src/Components/SelectTagsInFilter.js
+++ react-front-end/src/Components/SelectTagsInFilter.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 <link
   rel="stylesheet"
   href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
   integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
   crossorigin="anonymous"
-/>
+/>;
 
 export default function SelectTagsInFilter(props) {
-
-  
-  const [newColor, setNewColor] = useState('transparent')
-  const [select, setSelect] = useState(false)
+  const [newColor, setNewColor] = useState("transparent");
+  const [select, setSelect] = useState(false);
 
   const buttonColor = (select) => {
-
     if (select) {
-      setSelect(false)
-      setNewColor('transparent')
+      setSelect(false);
+      setNewColor("transparent");
     } else {
-      setSelect(true)
-      setNewColor('red')
+      setSelect(true);
+      setNewColor("red");
     }
-  }
+  };
   return (
-    
-    <Button variant="outline-primary">Primary</Button>
-
-  )
+    <Button
+      onClick={() => {
+        props.handleTagClick(props.id);
+        buttonColor(select);
+      }}
+      style={{ backgroundColor: newColor }}
+    >
+      {props.name}
+    </Button>
+  );
 }
-
-
