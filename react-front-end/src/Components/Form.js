16ca337b7e9e8f24098d083f2cb129ffc2ceaa19
MattLuo90 <mattluo604@hotmail.com>
2021-07-08 22:31:53 -0700

    add logo

--- react-front-end/src/Components/Form.js
+++ react-front-end/src/Components/Form.js
@@ -11,6 +11,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import ImageListItem from '@material-ui/core/ImageListItem';
 
 function Copyright() {
   return (
@@ -70,9 +71,9 @@ export default function Form(props) {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
+        <ImageListItem cols={1}>
+          <img src='../doc/logo.jpg' />
+        </ImageListItem>
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
