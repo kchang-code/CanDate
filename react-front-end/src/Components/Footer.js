9ac74a6332408943dfe8c9fd247b98be2949ee0f
Zio7711 <zi.ao.tan@outlook.com>
2021-07-11 17:34:28 -0700

    edit some social media icon

--- react-front-end/src/Components/Footer.js
+++ react-front-end/src/Components/Footer.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import './Footer.scss';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-function Footer() {
-  return <div></div>;
-}
+const Footer = () => {
+  return (
+    <div>
+      <body>
+        <div class="footer-basic">
+          <FacebookIcon />
+          <YouTubeIcon />
+          <TwitterIcon />
+          <LinkedInIcon />
+          <footer>
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a>Home</a>
+              </li>
+              <li class="list-inline-item">
+                <a>Contact Us</a>
+              </li>
+              <li class="list-inline-item">
+                <a>Log in</a>
+              </li>
+              <li class="list-inline-item">
+                <a>Subscribe</a>
+              </li>
+            </ul>
+            <p class="copyright">Can Date © 2021</p>
+          </footer>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+      </body>
+    </div>
+  );
+};
 
 export default Footer;
